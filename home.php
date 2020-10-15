861b9d07edca91ef460a0b602f70051fd85d8cce
Nazibul Haq <46378233+Shitoke@users.noreply.github.com>
2020-10-15 20:28:00 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<form action="/action_page.php">
+  <fieldset>
+    <legend>Personalia:</legend>
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" value=""><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname" value=""><br><br>
+    <input type="submit" value="Submit">
+  </fieldset>
+</form>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<form action="loginaction/result.php" method = 'post'>
+<inputs type = "text" placeholder = "Username" required >
+<inputs type = "password" placeholder = "Password" required >
+<input type = "submit" value ="Submit">
+</form>
+
+</body>
+</html>
